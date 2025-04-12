4a9a44056b87dcc152ae855a5d8089f179c58995
nikhilsabu3 <nikhilsabu3@gmail.com>
2025-04-12 19:48:42 +0100

    Little Lemon DB Files

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Employees` (
   `Role` VARCHAR(45) NOT NULL,
   `ContactNumber` VARCHAR(45) NOT NULL,
   `Email` VARCHAR(255) NOT NULL,
-  `AnnualSalary` DECIMAL NOT NULL,
+  `AnnualSalary` DECIMAL(10,2) NOT NULL,
   `AddressID` INT NOT NULL,
   PRIMARY KEY (`EmployeeID`),
   INDEX `AddressID_idx` (`AddressID` ASC) VISIBLE,
@@ -103,21 +103,61 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `LittleLemonDB`.`MenuItems`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItems` (
+  `MenuItemID` INT NOT NULL AUTO_INCREMENT,
+  `CourseName` VARCHAR(45) NOT NULL,
+  `StarterName` VARCHAR(45) NOT NULL,
+  `DessertName` VARCHAR(45) NULL,
+  `DrinkName` VARCHAR(45) NOT NULL,
+  `SidesName` VARCHAR(45) NOT NULL,
+  `MenuItemPrice` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`MenuItemID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Menus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menus` (
+  `MenuID` INT NOT NULL AUTO_INCREMENT,
+  `MenuItemsID` INT NOT NULL,
+  `MenuName` VARCHAR(45) NOT NULL,
+  `Cuisine` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`MenuID`),
+  INDEX `MenuItemsID_idx` (`MenuItemsID` ASC) VISIBLE,
+  CONSTRAINT `MenuItemsID`
+    FOREIGN KEY (`MenuItemsID`)
+    REFERENCES `LittleLemonDB`.`MenuItems` (`MenuItemID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `LittleLemonDB`.`Orders`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   `OrderID` VARCHAR(45) NOT NULL,
   `OrderDate` DATE NOT NULL,
   `BookingID` INT NOT NULL,
-  `BillAmount` DECIMAL NOT NULL,
+  `BillAmount` DECIMAL(10,2) NOT NULL,
   `SaleQuantity` INT NOT NULL,
-  `TotalCost` DECIMAL NULL,
-  `Discount` DECIMAL NULL DEFAULT 0,
+  `TotalCost` DECIMAL(10,2) NULL,
+  `Discount` DECIMAL(10,2) NULL DEFAULT 0,
+  `MenuID` INT NOT NULL,
   PRIMARY KEY (`OrderID`),
   INDEX `BookingID_idx` (`BookingID` ASC) VISIBLE,
+  INDEX `MenuID_idx` (`MenuID` ASC) VISIBLE,
   CONSTRAINT `BookingID`
     FOREIGN KEY (`BookingID`)
     REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`MenuID`)
+    REFERENCES `LittleLemonDB`.`Menus` (`MenuID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -131,7 +171,7 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Delivery` (
   `DeliveryDate` DATE NOT NULL,
   `DeliveryMode` VARCHAR(45) NULL,
   `DeliveryStatus` VARCHAR(45) NULL,
-  `DeliveryCost` DECIMAL NULL DEFAULT 0,
+  `DeliveryCost` DECIMAL(10,2) NULL DEFAULT 0,
   `OrderID` VARCHAR(45) NOT NULL,
   `AddressID` INT NOT NULL,
   PRIMARY KEY (`DeliveryID`),
@@ -145,59 +185,6 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Delivery` (
   CONSTRAINT `AddressID`
     FOREIGN KEY (`AddressID`)
     REFERENCES `LittleLemonDB`.`Address` (`AddressID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Menus`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menus` (
-  `MenuID` INT NOT NULL AUTO_INCREMENT,
-  `Cuisine` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`MenuID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`MenuItems`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItems` (
-  `ItemID` INT NOT NULL AUTO_INCREMENT,
-  `ItemName` VARCHAR(45) NOT NULL,
-  `Course` VARCHAR(45) NOT NULL,
-  `ItemPrice` DECIMAL NOT NULL,
-  `MenuID` INT NOT NULL,
-  PRIMARY KEY (`ItemID`),
-  INDEX `MenuID_idx` (`MenuID` ASC) VISIBLE,
-  CONSTRAINT `MenuID`
-    FOREIGN KEY (`MenuID`)
-    REFERENCES `LittleLemonDB`.`Menus` (`MenuID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`OrderDetails`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDetails` (
-  `OrderDetailsID` INT NOT NULL AUTO_INCREMENT,
-  `OrderID` VARCHAR(45) NOT NULL,
-  `OrderLineNo` INT NOT NULL,
-  `ItemID` INT NOT NULL,
-  PRIMARY KEY (`OrderDetailsID`),
-  INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
-  INDEX `ItemID_idx` (`ItemID` ASC) VISIBLE,
-  CONSTRAINT `OrderID2`
-    FOREIGN KEY (`OrderID`)
-    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `ItemID`
-    FOREIGN KEY (`ItemID`)
-    REFERENCES `LittleLemonDB`.`MenuItems` (`ItemID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -278,25 +265,16 @@ COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `LittleLemonDB`.`Orders`
+-- Data for table `LittleLemonDB`.`MenuItems`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `LittleLemonDB`;
-INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`) VALUES ('1', '25/08/2020', 2, 353, 3, 200, 20);
-INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`) VALUES ('2', '17/08/2021', 3, 113, 2, 51, 0);
-INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`) VALUES ('3', '15/06/2020', 1, 188, 4, 43, 9);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `LittleLemonDB`.`Delivery`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `LittleLemonDB`;
-INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (1, '26/03/2020', 'Truck', 'Deliver Completed', 61, '1', 2);
-INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (2, '17/07/2020', 'Hotel', 'In Progress', 97, '2', 1);
-INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (3, '24/04/2020', 'Hotel', 'Preparing to Deliver', 36, '3', 3);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (1, 'Greek salad', 'Olives', 'Greek yoghurt', 'Athens White wine', 'Tapas', 54);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (2, 'Bean soup', 'Flatbread', 'Ice cream', 'Corfu Red Wine', 'Potato salad', 69);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (3, 'Pizza', 'Minestrone', 'Cheesecake', 'Italian Coffee', 'Bruschetta', 34);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (4, 'Carbonara', 'Tomato bread', 'Affogato', 'Roma Red wine', 'Focaccia', 55);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (5, 'Kabasa', 'Falafel', 'Turkish yoghurt', 'Ankara White Wine', 'Meatballs', 78);
+INSERT INTO `LittleLemonDB`.`MenuItems` (`MenuItemID`, `CourseName`, `StarterName`, `DessertName`, `DrinkName`, `SidesName`, `MenuItemPrice`) VALUES (6, 'Shwarma', 'Hummus', 'Baklava', 'Turkish Coffee', 'Fries', 90);
 
 COMMIT;
 
@@ -306,52 +284,36 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `LittleLemonDB`;
-INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `Cuisine`) VALUES (1, 'Greek');
-INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `Cuisine`) VALUES (2, 'Italian');
-INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `Cuisine`) VALUES (3, 'Turkish');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (1, 1, 'Greek Heaven Delight', 'Greek');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (2, 4, 'Italian Relief', 'Italian');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (3, 5, 'Turkish Turn', 'Turkish');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (4, 2, 'Zeus Dinner', 'Greek');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (5, 3, 'Pizza For Life', 'Italian');
+INSERT INTO `LittleLemonDB`.`Menus` (`MenuID`, `MenuItemsID`, `MenuName`, `Cuisine`) VALUES (6, 6, 'Evening Share', 'Turkish');
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `LittleLemonDB`.`MenuItems`
+-- Data for table `LittleLemonDB`.`Orders`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `LittleLemonDB`;
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (1, 'Olives', 'Starters', 5, 1);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (2, 'Flatbread', 'Starters', 5, 1);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (3, 'Minestrone', 'Starters', 8, 2);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (4, 'Tomato bread', 'Starters', 8, 2);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (5, 'Falafel', 'Starters', 7, 3);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (6, 'Hummus', 'Starters', 5, 3);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (7, 'Greek salad', 'Main Courses', 15, 1);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (8, 'Bean soup', 'Main Courses', 12, 2);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (9, 'Pizza', 'Main Courses', 15, 3);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (10, 'Greek yoghurt', 'Desserts', 7, 1);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (11, 'Ice cream', 'Desserts', 6, 2);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (12, 'Cheesecake', 'Desserts', 5, 3);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (13, 'Athens White wine', 'Drinks', 25, 1);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (14, 'Corfu Red Wine', 'Drinks', 30, 2);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (15, 'Turkish Coffee', 'Drinks', 10, 3);
-INSERT INTO `LittleLemonDB`.`MenuItems` (`ItemID`, `ItemName`, `Course`, `ItemPrice`, `MenuID`) VALUES (16, 'Kabasa', 'Main Courses', 17, 3);
+INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`, `MenuID`) VALUES ('1', '2020-08-05', 2, 353.50, 5, 200.00, 20.00, 4);
+INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`, `MenuID`) VALUES ('2', '2021-08-17', 3, 211.00, 3, 150.30, 0.00, 2);
+INSERT INTO `LittleLemonDB`.`Orders` (`OrderID`, `OrderDate`, `BookingID`, `BillAmount`, `SaleQuantity`, `TotalCost`, `Discount`, `MenuID`) VALUES ('3', '2020-06-15', 1, 188.75, 1, 77.70, 9.5, 3);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `LittleLemonDB`.`OrderDetails`
+-- Data for table `LittleLemonDB`.`Delivery`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `LittleLemonDB`;
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (1, '1', 1, 4);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (2, '1', 2, 8);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (3, '1', 3, 11);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (4, '2', 1, 2);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (5, '2', 2, 7);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (6, '3', 1, 5);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (7, '3', 2, 9);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (8, '3', 3, 12);
-INSERT INTO `LittleLemonDB`.`OrderDetails` (`OrderDetailsID`, `OrderID`, `OrderLineNo`, `ItemID`) VALUES (9, '3', 4, 16);
+INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (1, '2020-03-26', 'Truck', 'Deliver Completed', 61.50, '1', 2);
+INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (2, '2020-07-17', 'Hotel', 'In Progress', 97.00, '2', 1);
+INSERT INTO `LittleLemonDB`.`Delivery` (`DeliveryID`, `DeliveryDate`, `DeliveryMode`, `DeliveryStatus`, `DeliveryCost`, `OrderID`, `AddressID`) VALUES (3, '2020-04-24', 'Hotel', 'Preparing to Deliver', 36.30, '3', 3);
 
 COMMIT;
 
